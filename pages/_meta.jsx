bd308f314674ff13976e249323f82bd88973fdd5
Rimaz <66942996+Rimaz26@users.noreply.github.com>
2025-01-27 13:01:11 +0530

    social-media-links-updated

--- pages/_meta.jsx
+++ pages/_meta.jsx
@@ -10,31 +10,8 @@ export default {
     type: 'page',
     "display": "hidden"
   },
-  features: {
-    title: 'Features',
-    type: 'page',
-    href: 'https://docs.rapidload.io/features',
-    // "display": "hidden"
-  },
-  pricing: {
-    title: 'Pricing',
-    type: 'page',
-    href: 'https://docs.rapidload.io/pricing',
-    // "display": "hidden"
-  },
-  contactus: {
-    title: 'Contact Us',
-    type: 'page',
-    href: 'https://docs.rapidload.io/contactus',
-    // "display": "hidden"
-  },
-  support: {
-    title: 'Support',
-    type: 'page',
-    href: 'https://docs.rapidload.io/support',
-    // "display": "hidden"
-  },
-  "version": {
+  
+"version": {
     "title": "Version",
     "type": "menu",
     "items": {
@@ -46,12 +23,31 @@ export default {
       }
     }
   },
-  github: {
+
+  Download: {
+    title: 'Download',
+    type: 'page',
+    href: 'https://wordpress.org/plugins/unusedcss/',
+    // "display": "hidden"
+  },
+  Account: {
+    title: 'Account',
+    type: 'page',
+    href: 'https://app.rapidload.io/',
+    // "display": "hidden"
+  },
+  SignUp: {
+    title: 'Sign Up',
+    type: 'page',
+    href: 'https://rapidload.io/signup/',
+    // "display": "hidden"
+  },
+  Twitter: {
     title: <svg width="16" height="21" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M12.3189 0.595459H14.5628L9.66175 6.01786L15.4278 13.3966H10.9135L7.37766 8.92064L3.33157 13.3966H1.08679L6.32887 7.59638L0.797852 0.595459H5.42731L8.62303 4.68566L12.3189 0.595459ZM11.5326 12.0971H12.7761L4.75067 1.82702H3.41752L11.5326 12.0971Z" fill="black"/>
     </svg>,
     type: 'page',
-    href: 'https://github.com/rapidload-io/docs',
+    href: 'https://x.com/_rapidload',
   },
 
 }
