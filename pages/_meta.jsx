940c66770e09307af073264718eb9c9371d76ef9
shakee93 <shakee.zats@gmail.com>
2025-01-27 16:32:18 +0530

    fix: cursor fix

--- pages/_meta.jsx
+++ pages/_meta.jsx
@@ -1,5 +1,6 @@
 
 export default {
+ 
   getting_started: {
     title: 'Getting Started',
     sidebar: true,
@@ -42,8 +43,8 @@ export default {
     type: 'page',
     "display": "hidden"
   },
-  
-"version": {
+
+  "version": {
     "title": "Version",
     "type": "menu",
     "items": {
@@ -76,7 +77,7 @@ export default {
   },
   Twitter: {
     title: <svg width="16" height="21" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12.3189 0.595459H14.5628L9.66175 6.01786L15.4278 13.3966H10.9135L7.37766 8.92064L3.33157 13.3966H1.08679L6.32887 7.59638L0.797852 0.595459H5.42731L8.62303 4.68566L12.3189 0.595459ZM11.5326 12.0971H12.7761L4.75067 1.82702H3.41752L11.5326 12.0971Z" fill="black"/>
+      <path d="M12.3189 0.595459H14.5628L9.66175 6.01786L15.4278 13.3966H10.9135L7.37766 8.92064L3.33157 13.3966H1.08679L6.32887 7.59638L0.797852 0.595459H5.42731L8.62303 4.68566L12.3189 0.595459ZM11.5326 12.0971H12.7761L4.75067 1.82702H3.41752L11.5326 12.0971Z" fill="black" />
     </svg>,
     type: 'page',
     href: 'https://x.com/_rapidload',
